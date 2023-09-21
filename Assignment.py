899601951ca2c89b80f8efd445ceb756098f46c8
Enamul87 <144311413+Enamul87@users.noreply.github.com>
2023-09-21 09:26:12 +0900

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,38 @@
+print(int(3.141595))
+print(float(365))
+print(str(50))
+print(float(99) + 100)
+my_string = "hello" + "there"
+print(type(my_string))
+my_string = my_string + 1
+age = input ("Enter your age :" )
+print(type(age))
+age = int(input("Enter your age : "))
+print(type(age))
+width = 17
+Height = 12.0
+x = width//2
+print(x)
+y = width / 2.0
+print(y)
+z = Height /3
+print(z)
+s = 1 + 2 * 5
+print(s)
+x = input("Enter ytour name : ")
+y = ("Hello "+ x)
+print (y)
+x = input("enter hours:")
+y = input("enter rate :")
+z= x*y
+print("salery "+ z)
+Celsius = int(input('Enter the temperture : '))
+Fahrenheit = Celsius * 9 / 5 + 32
+print(Fahrenheit)
+
+
+
+x= float(input("enter time in 'seconds':" ))
+y = x/60
+z= x/3600
+print (x, " is ",y, "minutes and", z, "hours")
